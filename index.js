e78cb909c6a2429b72946156636261190fccf86d
Damon Murdoch <Damon.Murdoch@qed.qld.gov.au>
2022-02-18 15:34:16 +1000

    lol

--- index.js
+++ index.js
@@ -7,25 +7,91 @@ const params = new URLSearchParams(query);
 // Show the deck page
 function showPageDeck(year, format, deck)
 {
+  // Set the title to the format + the team
+  let title = format + ' - ' + deck;
 
+  // Set the document title to the title
+  document.title = title;
+
+  // Set the name of the site to the title
+  document.getElementById('sitesubtitle').innerHTML = title;
 }
 
 // Show the format page
 function showPageFormat(year, format)
 {
+  // Set the title to the year + the format
+  let title = year + ' - ' + format;
 
+  // Set the document title to the title
+  document.title = title;
+
+  // Set the name of the site to the title
+  document.getElementById('sitesubtitle').innerHTML = title;
 }
 
 // Show the year page
 function showPageYear(year)
 {
+  // Set the title to the year
+  let title = year;
 
+  // Set the document title to the title
+  document.title = title;
+  
+  // Set the name of the site to the title
+  document.getElementById('sitesubtitle').innerHTML = title;
 }
 
 // Show the home page
 function showPageHome()
 {
+  // Get the page body from the content
+  let body = document.getElementById('content');
+
+  // Set the title to home
+  let title = 'Home';
+
+  // Set the document title to the title
+  document.title = title;
   
+  // Set the name of the site to the title
+  document.getElementById('sitesubtitle').innerHTML = title;
+
+  // Div for controlling the width of a container
+  let div = document.createElement('div');
+
+  // Set the width of the div element
+  // div.classList.value = "w-50";
+  div.classList.value = "text-center"
+
+  // Select drop-down for the year
+  let select = document.createElement('select');
+
+  // Loop over all of the years in the decks
+  for(let key of Object.keys(DECKS))
+  {
+    // Create the table element
+    let option = document.createElement('option');
+
+    // Add a row linking to the year to the table
+    option.innerHTML = "<a class='link-light' href='?year=" + key + "'>" + key + "</a>";
+
+    // Create the onclick event
+    option.onclick = function a(){
+      // Open the page for the option linked
+      window.location.href = "?year= " + option.value;
+    };
+
+    // Add the row to the body
+    select.appendChild(option);
+  }
+
+  // Add the document to the table
+  div.appendChild(select);
+
+  // Add the div to the body
+  body.appendChild(div);
 }
 
 // If year is selected
